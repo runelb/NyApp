8fbba6fb6af8b9f5c6f3218926e6dc6f29a3a093
runelb <rune@jidrot.se>
2017-08-28 01:52:39 +0200

    Initial commit

---
+++ app/src/main/java/com/example/nyapp/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.nyapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // called when user taps send button
+    // public called bu onClick, from outside
+    // void, return nothing
+    // param is the view (widget) in question, button
+    public void sendMessage(View view) {
+        // do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+    }
+}
